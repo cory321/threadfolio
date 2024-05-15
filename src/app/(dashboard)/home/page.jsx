3855d30060f573b968fa8cced148c4290fcae316
Cory Williams <cory321@gmail.com>
2024-05-14 17:47:04 -0700

    update loading spinner for todo list

--- src/app/(dashboard)/home/page.jsx
+++ src/app/(dashboard)/home/page.jsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { useUser } from '@clerk/nextjs'
+import CircularProgress from '@mui/material/CircularProgress'
+import Box from '@mui/material/Box'
 
 import AddTodoForm from '@/components/todo/AddTodoForm'
 import TodoList from '@/components/todo/TodoList'
@@ -10,26 +12,29 @@ import TodoList from '@/components/todo/TodoList'
 export default function Home() {
   const { isSignedIn, isLoading, user } = useUser()
   const [todos, setTodos] = useState(null)
+  const [authLoading, setAuthLoading] = useState(true)
+
+  useEffect(() => {
+    if (!isLoading) {
+      setAuthLoading(false)
+    }
+  }, [isLoading])
+
+  if (authLoading) {
+    return <CircularProgress />
+  }
+
+  if (!isSignedIn) {
+    return null // or you can redirect to sign-in page or show a message
+  }
 
   return (
-    <>
-      {isLoading ? (
-        <></>
-      ) : (
-        <main>
-          <div>
-            {isSignedIn ? (
-              <>
-                <h1>Welcome {user.firstName}</h1>
-                <AddTodoForm todos={todos} setTodos={setTodos} />
-                <TodoList todos={todos} setTodos={setTodos} />
-              </>
-            ) : (
-              <div>Sign in to create your todo list!</div>
-            )}
-          </div>
-        </main>
-      )}
-    </>
+    <main>
+      <div>
+        <h1>Welcome {user.firstName}</h1>
+        <AddTodoForm todos={todos} setTodos={setTodos} />
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
+    </main>
   )
 }
