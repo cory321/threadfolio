5f2f741c676ac34e3cbca4d1678105f740e8ae82
Cory Williams <cory321@gmail.com>
2024-10-01 21:40:41 -0700

    change user setup and routes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,18 @@
-// Next Imports
-import { redirect } from 'next/navigation'
+import Link from 'next/link'
 
-export default function Page() {
-  redirect('/home')
+export default function LandingPage() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link href='/login'>Login</Link>
+          <Link href='/register'>Register</Link>
+        </nav>
+      </header>
+      <main>
+        <h1>Welcome to Your App</h1>
+        <p>This is the public landing page accessible by everyone.</p>
+      </main>
+    </div>
+  )
 }
