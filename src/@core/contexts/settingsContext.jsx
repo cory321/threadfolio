74996c29c44674904ebebe6abfc7ad8068986198
Cory Williams <cory321@gmail.com>
2024-10-04 17:10:45 -0700

    upgrade next.js to 14.2

--- src/@core/contexts/settingsContext.jsx
+++ src/@core/contexts/settingsContext.jsx
@@ -1,22 +1,15 @@
 'use client'
-import { createContext, useMemo, useState } from 'react'
+import React, { createContext, useState } from 'react'
 
 // Config Imports
 import themeConfig from '@configs/themeConfig'
-import demoConfigs from '@configs/demoConfigs'
 import primaryColorConfig from '@configs/primaryColorConfig'
-
-// Hook Imports
-import { useObjectCookie } from '@core/hooks/useObjectCookie'
 
 // Initial Settings Context
 export const SettingsContext = createContext(null)
 
 // Settings Provider
-export const SettingsProvider = props => {
-  const demoName = props.demoName || null
-  const demoConfigurations = demoName ? demoConfigs[demoName] : {}
-
+export const SettingsProvider = ({ children }) => {
   // Initial Settings
   const initialSettings = {
     mode: themeConfig.mode,
@@ -26,79 +19,17 @@ export const SettingsProvider = props => {
     navbarContentWidth: themeConfig.navbar.contentWidth,
     contentWidth: themeConfig.contentWidth,
     footerContentWidth: themeConfig.footer.contentWidth,
-    primaryColor: primaryColorConfig[0].main,
-    ...(demoName && demoConfigurations)
+    primaryColor: primaryColorConfig[0].main
   }
 
-  const updatedInitialSettings = {
-    ...initialSettings,
-    mode: props.mode || (demoName && demoConfigurations.mode) || themeConfig.mode
+  const [settings, setSettings] = useState(initialSettings)
+
+  const updateSettings = updatedSettings => {
+    setSettings(prevSettings => ({
+      ...prevSettings,
+      ...updatedSettings
+    }))
   }
 
-  // Cookies
-  const [settingsCookie, updateSettingsCookie] = useObjectCookie(
-    demoName ? themeConfig.settingsCookieName.replace('demo-1', demoName) : themeConfig.settingsCookieName,
-    JSON.stringify(props.settingsCookie) !== '{}' ? props.settingsCookie : updatedInitialSettings
-  )
-
-  // State
-  const [_settingsState, _updateSettingsState] = useState(
-    JSON.stringify(settingsCookie) !== '{}' ? settingsCookie : updatedInitialSettings
-  )
-
-  const updateSettings = (settings, options) => {
-    const { updateCookie = true } = options || {}
-
-    _updateSettingsState(prev => {
-      const newSettings = { ...prev, ...settings }
-
-      // Update cookie if needed
-      if (updateCookie) updateSettingsCookie(newSettings)
-
-      return newSettings
-    })
-  }
-
-  /**
-   * Updates the settings for page with the provided settings object.
-   * Updated settings won't be saved to cookie hence will be reverted once navigating away from the page.
-   *
-   * @param settings - The partial settings object containing the properties to update.
-   * @returns A function to reset the page settings.
-   *
-   * @example
-   * useEffect(() => {
-   *     return updatePageSettings({ theme: 'dark' });
-   * }, []);
-   */
-  const updatePageSettings = settings => {
-    updateSettings(settings, { updateCookie: false })
-
-    // Returns a function to reset the page settings
-    return () => updateSettings(settingsCookie, { updateCookie: false })
-  }
-
-  const resetSettings = () => {
-    updateSettings(initialSettings)
-  }
-
-  const isSettingsChanged = useMemo(
-    () => JSON.stringify(initialSettings) !== JSON.stringify(_settingsState),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [_settingsState]
-  )
-
-  return (
-    <SettingsContext.Provider
-      value={{
-        settings: _settingsState,
-        updateSettings,
-        isSettingsChanged,
-        resetSettings,
-        updatePageSettings
-      }}
-    >
-      {props.children}
-    </SettingsContext.Provider>
-  )
+  return <SettingsContext.Provider value={{ settings, updateSettings }}>{children}</SettingsContext.Provider>
 }
