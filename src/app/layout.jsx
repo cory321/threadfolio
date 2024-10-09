47bef97263ec85bc04d5e95314e34be8b8d83ce7
Cory Williams <cory321@gmail.com>
2024-10-09 03:07:16 -0700

    move clerk provider to root temporarily

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from '@clerk/nextjs'
+
 // Third-party Imports
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
@@ -11,17 +13,31 @@ export const metadata = {
   title: 'Threadfolio - An app for seamstresses and tailors!',
   description: 'Threadfolio is an app for seamstresses and tailors to manage their clients, projects, and more!'
 }
-
 const primaryColor = '#8C57FF'
+
+const appearance = {
+  variables: {
+    colorPrimary: primaryColor
+  }
+}
 
 const RootLayout = ({ children }) => {
   // Vars
   const direction = 'ltr'
 
   return (
-    <html id='__next' lang='en' dir={direction}>
-      <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      signInUrl='/login'
+      signUpUrl='/register'
+      signInFallbackRedirectUrl='/dashboard'
+      signUpFallbackRedirectUrl='/onboarding'
+      appearance={appearance}
+    >
+      <html id='__next' lang='en' dir={direction}>
+        <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
 
--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,5 +1,4 @@
 // Context Imports
-import { ClerkProvider } from '@clerk/nextjs'
 
 import { VerticalNavProvider } from '@menu/contexts/verticalNavContext'
 import { SettingsProvider } from '@core/contexts/settingsContext'
@@ -14,14 +13,6 @@ import AppReactToastify from '@/libs/styles/AppReactToastify'
 // Util Imports
 import { getDemoName, getMode, getSettingsFromCookie, getSystemMode } from '@core/utils/serverHelpers'
 
-const primaryColor = '#8C57FF'
-
-const appearance = {
-  variables: {
-    colorPrimary: primaryColor
-  }
-}
-
 const Providers = props => {
   // Props
   const { children, direction } = props
@@ -33,23 +24,14 @@ const Providers = props => {
   const systemMode = getSystemMode()
 
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      signInUrl='/login'
-      signUpUrl='/register'
-      signInFallbackRedirectUrl='/dashboard'
-      signUpFallbackRedirectUrl='/onboarding'
-      appearance={appearance}
-    >
-      <VerticalNavProvider>
-        <SettingsProvider settingsCookie={settingsCookie} mode={mode} demoName={demoName}>
-          <ThemeProvider direction={direction} systemMode={systemMode}>
-            {children}
-            <AppReactToastify position={themeConfig.toastPosition} hideProgressBar rtl={direction === 'rtl'} />
-          </ThemeProvider>
-        </SettingsProvider>
-      </VerticalNavProvider>
-    </ClerkProvider>
+    <VerticalNavProvider>
+      <SettingsProvider settingsCookie={settingsCookie} mode={mode} demoName={demoName}>
+        <ThemeProvider direction={direction} systemMode={systemMode}>
+          {children}
+          <AppReactToastify position={themeConfig.toastPosition} hideProgressBar rtl={direction === 'rtl'} />
+        </ThemeProvider>
+      </SettingsProvider>
+    </VerticalNavProvider>
   )
 }
 
