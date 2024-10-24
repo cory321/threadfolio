31cbf6e85f9d809fbb6e146aadaf620b0187f5c5
Cory Williams <cory321@gmail.com>
2024-10-24 16:20:30 -0700

    fix lint issue

--- src/app/apps/user/view/user-right/orders/index.jsx
+++ src/app/apps/user/view/user-right/orders/index.jsx
@@ -1,21 +1,8 @@
 'use client'
 
-// MUI Imports
 import dynamic from 'next/dynamic'
 
-import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
-
-// Style Imports
-import tableStyles from '@core/styles/table.module.css'
-
-// Dynamic Imports
-const GarmentListTable = dynamic(() => import('@/app/apps/user/view/user-right/GarmentListTable'))
+const GarmentListTable = dynamic(() => import('@/app/apps/user/view/user-right/overview/GarmentListTable'))
 
 const OrdersTab = () => {
   return <GarmentListTable />
