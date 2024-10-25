cadf426e4ee7ceb1df18e1be3e35fe73978ae336
Cory Williams <cory321@gmail.com>
2024-10-25 13:52:34 -0700

    add settings fix

--- src/views/front-pages/landing-page/index.jsx
+++ src/views/front-pages/landing-page/index.jsx
@@ -13,19 +13,20 @@ import ProductStat from './ProductStat'
 import Faqs from './Faqs'
 import GetStarted from './GetStarted'
 import ContactUs from './ContactUs'
-import { useSettings } from '@core/hooks/useSettings'
+
+// import { useSettings } from '@core/hooks/useSettings'
 
 const LandingPageWrapper = ({ mode, userId, token }) => {
   // Hooks
-  const { updatePageSettings } = useSettings()
+  // const { updatePageSettings } = useSettings()
 
-  // For Page specific settings
-  useEffect(() => {
-    return updatePageSettings({
-      skin: 'default'
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  // // For Page specific settings
+  // useEffect(() => {
+  //   return updatePageSettings({
+  //     skin: 'default'
+  //   })
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   return (
     <>
